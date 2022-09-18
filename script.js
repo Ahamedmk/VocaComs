d1e3be5a386240f71c493b5be827b8d31b545473
Ahamedmk <amkavavo1@gmail.com>
2022-09-19 01:26:33 +0200

    perpesctive

---
+++ script.js
@@ -0,0 +1,5 @@
+const faEye = document.querySelector(".fa-eye-slash");
+const doubleFace = document.querySelector(".double-face");
+faEye.addEventListener("click", function(){
+doubleFace.classList.toggle("active");
+})
