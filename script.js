16a0317d6b6b142d7181571f88c52c4cef0cce14
Ahamedmk <amkavavo1@gmail.com>
2022-09-19 23:34:11 +0200

    mettre le logo

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+const alimentation = [
+  {
+    motComorien: "Hula",
+    motFr: "Manger",
+  },
+  {
+    motComorien: "Apvasa",
+    motFr: "Maintenant",
+  },
+  {
+    motComorien: "Darasa",
+    motFr: "leçon(s)",
+  },
+  {
+    motComorien: "Djibu",
+    motFr: "répond",
+  },
+  {
+    motComorien: "Fasiri",
+    motFr: "traduire",
+  }
+];
+
+console.log(alimentation.length);
 const faEye = document.querySelector(".fa-eye-slash");
 const doubleFace = document.querySelector(".double-face");
-faEye.addEventListener("click", function(){
-doubleFace.classList.toggle("active");
-})
+
+
+faEye.addEventListener("click", function () {
+  doubleFace.classList.toggle("active");
+});
+
+// function createCard (){
+
+// }
