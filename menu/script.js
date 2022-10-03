c2da1ac64665c5614869a048b9f65bc980c40b28
Ahamedmk <amkavavo1@gmail.com>
2022-10-04 01:29:51 +0200

    menu4

--- menu/script.js
+++ menu/script.js
@@ -2,5 +2,5 @@ const vocab = document.querySelector(".abc");
 
 vocab.addEventListener("click", function(){
   console.log(vocab);
-  location.assign("/page quizz/index.html")
+  location.assign("../page_quizz/index.html")
 })
